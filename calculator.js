833e2f769b46c573ecc6a3824d1bd8a1587bdf9b
Harvey <arvymd@yahoo.com.ph>
2021-05-25 20:22:20 +0000

    Create BMI Calculator

--- calculator.js
+++ calculator.js
@@ -16,6 +16,18 @@ app.post("/", function (req, res) {
   res.send("The result is " + result);
 })
 
+app.get("/bmicalculator", function (req, res) {
+  res.sendFile(__dirname + "/bmicalculator.html");
+});
+
+app.post("/bmicalculator", function (req, res) {
+  const weight = parseFloat(req.body.weight);
+  const height = parseFloat(req.body.height);
+  const result = Math.floor(weight/(height*height));
+
+  res.send("Your BMI is " + result);
+})
+
 app.listen(3000, function () {
   console.log("Server running successfully.");
 })
